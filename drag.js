b1ab343a7ea33ee823d56447fea1d5f8fc1772e8
hrjhrj001 <271347808@qq.com>
2015-08-23 09:25:21 +0800

    two

--- drag.js
+++ drag.js
@@ -1,6 +1,8 @@
 function drag(id) {
 	var obj=document.getElementById(id);
 	obj.onmousedown=function(ev){
+		var disx=0;
+		var disy=0;
 		var ev=ev||window.event;
 		document.onmousemove=function(){
 				
